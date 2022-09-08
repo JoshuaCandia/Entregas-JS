a3218d4e9c73ca34a688005c54b80ecaa90cc70f
Joshua Candia <joshuacandia74@gmail.com>
2022-09-07 23:22:12 -0300

    .

--- js/entregaActual/app.js
+++ js/entregaActual/app.js
@@ -1,20 +1,34 @@
-import {opcionDeCompra,calculoPago,opcionesDePago,opcion} from "./modules.js";
-import {producto1Obj,producto2Obj,producto3Obj,producto4Obj} from "./modules.js"
+import {
+  opcionDeCompra,
+  calculoPago,
+  opcionesDePago,
+  opcion,
+} from "./modules.js";
+import {
+  producto1Obj,
+  producto2Obj,
+  producto3Obj,
+  producto4Obj,
+} from "./modules.js";
 
 let condicion = true;
 while (condicion) {
   let producto = opcionDeCompra();
 
-  if (  producto == producto1Obj || producto == producto2Obj || producto == producto3Obj || producto == producto4Obj  ) {
+  if (
+    producto == producto1Obj ||
+    producto == producto2Obj ||
+    producto == producto3Obj ||
+    producto == producto4Obj
+  ) {
     let formaPago = opcionesDePago(producto);
 
     calculoPago(producto, formaPago);
     break;
-  } else if(producto == 5){
-    break
-  }else{
-    alert("No ingreso una opcion valida")
-    continue
+  } else if (producto == 5) {
+    break;
+  } else {
+    alert("No ingreso una opcion valida");
+    continue;
   }
 }
-
